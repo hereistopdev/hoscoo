e3e8b6fe2f0f0b2f5a03608d368f26116584cd46
peterjensendev <peter@or.xyz>
2024-09-11 21:50:02 +0200

    added forgetPass page

---
+++ src/layouts/authentication/forget-pass/index.js
@@ -0,0 +1,74 @@
+/**
+=========================================================
+* Hoscoo React - v4.0.1
+=========================================================
+
+* Product Page: https://www.hoscoo.com
+* Copyright 2024 Hoscoo (https://www.hoscoo.com)
+
+Coded by www.hoscoo.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+import { useState } from "react";
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// Hoscoo React components
+import SoftBox from "components/SoftBox";
+import SoftTypography from "components/SoftTypography";
+import SoftInput from "components/SoftInput";
+import SoftButton from "components/SoftButton";
+
+// Authentication layout components
+import CoverLayout from "layouts/authentication/components/CoverLayout";
+
+// Images
+import curved9 from "assets/images/curved-images/curved-6.jpg";
+
+function ForgetPass() {
+  return (
+    <CoverLayout
+      title="Forgot password?"
+      description="Enter your email below, you will receive an email with instructions on how to reset your password in a few mintues."
+      image={curved9}
+    >
+      <SoftBox component="form" role="form">
+        <SoftBox mb={2}>
+          <SoftBox mb={1} ml={0.5}>
+            <SoftTypography component="label" variant="caption" fontWeight="bold">
+              Email
+            </SoftTypography>
+          </SoftBox>
+          <SoftInput type="email" placeholder="Email" />
+        </SoftBox>
+        <SoftBox mt={4} mb={1}>
+          <SoftButton variant="gradient" color="info" fullWidth>
+            Start recovery
+          </SoftButton>
+        </SoftBox>
+        <SoftBox mt={3} textAlign="center">
+          <SoftTypography variant="button" color="text" fontWeight="regular">
+            You don&apos;t have an account?{" "}
+            <SoftTypography
+              component={Link}
+              to="/authentication/sign-up"
+              variant="button"
+              color="info"
+              fontWeight="medium"
+              textGradient
+            >
+              Create an account
+            </SoftTypography>
+          </SoftTypography>
+        </SoftBox>
+      </SoftBox>
+    </CoverLayout>
+  );
+}
+
+export default ForgetPass;
